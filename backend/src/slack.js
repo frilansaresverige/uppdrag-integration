6cbfbfcde81703ae51440a62a8a947f71ac29ea7
Martin Pola <martin@kottnet.net>
2025-02-26 21:50:55 +0100

    Låt uppdragsbeskrivningar ligga som svar (#14)

--- backend/src/slack.js
+++ backend/src/slack.js
@@ -15,33 +15,49 @@ exports.sync = async () => {
 exports.propagateAssignment = async assignmentId => {
   const assignment = await model.getAssignment(assignmentId)
 
-  const text = common.fillTemplate(config.templates.slackAssignment, assignment)
-
-  const params = new URLSearchParams()
-
-  params.append('token', config.slack.token)
-  params.append('channel', assignment.slackChannel)
-  params.append('text', text)
-
-  let ok, ts
-
-  try {
-    const response = await axios.post('https://slack.com/api/chat.postMessage', params)
-
-    ok = response.data.ok
-
-    if (ok) {
-      ts = response.data.ts
-    } else {
-      throw response.data
-    }
-  } catch (error) {
-    console.error('Failed to post assignment ' + assignmentId + ' to Slack:')
-    console.error(error)
+  const text = {
+    'INITIAL': common.fillTemplate(config.templates.slackAssignmentInitial, assignment),
+    'THREAD': common.fillTemplate(config.templates.slackAssignmentThread, assignment),
   }
 
-  if (ok) {
-    await model.setAssignmentSlackId(assignment.id, ts)
+  let threadId = null
+
+  for (const job of ['INITIAL', 'THREAD']) {
+    if (job === 'THREAD' && threadId === null) {
+      continue
+    }
+
+    const params = new URLSearchParams()
+
+    params.append('token', config.slack.token)
+    params.append('channel', assignment.slackChannel)
+    params.append('text', text[job])
+
+    if (job === 'THREAD') {
+      params.append('thread_ts', threadId)
+    }
+
+    let ok, ts
+
+    try {
+      const response = await axios.post('https://slack.com/api/chat.postMessage', params)
+
+      ok = response.data.ok
+
+      if (ok) {
+        ts = response.data.ts
+      } else {
+        throw response.data
+      }
+    } catch (error) {
+      console.error('Failed to post assignment ' + assignmentId + ' for ' + job + ' to Slack:')
+      console.error(error)
+    }
+
+    if (ok && job === 'INITIAL') {
+      await model.setAssignmentSlackId(assignment.id, ts)
+      threadId = ts
+    }
   }
 }
 
